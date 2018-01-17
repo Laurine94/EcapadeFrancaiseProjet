4f597c313ef96e2d04129b9f3e08e387992f4190
Windows <Windows@DESKTOP-BF3GK6N>
2018-01-17 15:50:01 +0100

    test

--- IHM_FR/EspaceClient/vues/v_accueilClient.php
+++ IHM_FR/EspaceClient/vues/v_accueilClient.php
@@ -3,35 +3,54 @@
 <?php
 ?>
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div id="imageEncadreBienvenue">
-
+    <div class="row" id="imageEncadreBienvenue">
+        <div class="col-md-12 col-sm-12 col-xs-12" >
                 <div class="col-md-12 col-xs-12" id="encadreBienvenue">
                     <h2 id="h2_encadreBienvenue">Bienvenue sur votre espace client <?php echo $prenom ;?></h2>
 
                 </div>
-                <div id="cadreEssentiels">
-                    <p>Retrouvez tous vos essentiels sur votre espace client</p>
+                <div class="row" id="cadreEssentiels">
+                    <div class="col-md-12 col-sm-12 col-xs-12"
+                        <p>Retrouvez tous vos essentiels sur votre espace client</p>
+                    </div>
                 </div>
-            </div>
         </div>
     </div>
     <div class="row" id="wishlist-factures">
         <div class="col-md-6 col-sm-6 col-xs-6" id="image-wishlist">
-            <div class="w-f-white-div">
-                <h1>Ma Wishlist</h1><br>
-                <hr class="hr"><br>
-                <h2>Tous vos coups de coeur pour vos futures séjours en France</h2>
-                <a href="#"><input type="button" class="btn link-w-f" name="btn-voirFactures" value="Voir"></a>
+            <div class="row w-f-white-div">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h1>Ma Wishlist</h1><br>
+                    <hr class="hr"><br>
+                    <h2>Tous vos coups de coeur pour vos futures séjours en France</h2><br>
+                    <a href="#"><input type="button" class="btn link-w-f" name="btn-voirFactures" value="Voir"></a>
+                </div>
             </div>
         </div>
         <div class="col-md-6 col-sm-12 col-xs-6" id="image-factures">
-            <div class="w-f-white-div">
-                <h1>Mes factures</h1><br>
-                <hr class="hr"><br>
-                <h2>Retrouvez toutes vos factures issus de vos dernières escapades!</h2>
-                <a href="indexClient.php?uc=espaceClient&action=voirFacture" type="button" ><input type="button" class="btn link-w-f" name="btn-voirFactures" value="Voir"></a>
+            <div class="row w-f-white-div">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <h1>Mes factures</h1><br>
+                    <hr class="hr"><br>
+                    <h2>Retrouvez toutes vos factures issus de vos dernières escapades!</h2><br>
+                    <a href="indexClient.php?uc=espaceClient&action=voirFacture" type="button" ><input type="button" class="btn link-w-f" name="btn-voirFactures" value="Voir"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-6" id="resa-a-venir">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-6 r_div" id="">
+                    <a><h1>Mes réservations à venir</h1></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6" id="resa-passees">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-6 r_div" id="">
+                    <a><h1>Mes réservations passées</h1></a>
+                </div>
             </div>
         </div>
     </div>
